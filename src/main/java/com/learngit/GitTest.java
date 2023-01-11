ec7e07bd2d98c11e99c4e2186bbd66e0d3178cbf
Lgl <c9553632673419@qq.com>
2023-01-11 12:44:51 +0800

    second commit

--- src/main/java/com/learngit/GitTest.java
+++ src/main/java/com/learngit/GitTest.java
@@ -5,5 +5,7 @@ package com.learngit;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+
     }
 }
